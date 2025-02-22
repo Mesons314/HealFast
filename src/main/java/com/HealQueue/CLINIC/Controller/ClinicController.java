1ba8ae38a7cbacd7cd557c86ef2523861ee20d45
Abhinav Dwivedi <photon278@gmail.com>
2025-02-22 15:13:11 +0530

    Checking Out

--- src/main/java/com/HealQueue/CLINIC/Controller/ClinicController.java
+++ src/main/java/com/HealQueue/CLINIC/Controller/ClinicController.java
@@ -1,16 +1,32 @@
 package com.HealQueue.CLINIC.Controller;
 
+import com.HealQueue.Auth.Entity.ClinicInfo;
+import com.HealQueue.Auth.Service.ClinicService;
+import com.HealQueue.CLINIC.DTO.ClinicResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/clinic")
 public class ClinicController {
 
+    @Autowired
+    private ClinicService clinicService;
     @GetMapping("/clinicData")
     public String getDataById(){
         return "";
+    }
+
+    @GetMapping("/clinicData/{id}")
+    public ResponseEntity<List<ClinicInfo>> getClinicList(){
+        return new ResponseEntity<>(clinicService.getClinic(), HttpStatus.OK);
     }
 
 }
