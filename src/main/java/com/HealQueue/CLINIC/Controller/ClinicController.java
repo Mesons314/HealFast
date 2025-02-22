adb637b341deec7812815913083859f4f5745939
Abhinav Dwivedi <photon278@gmail.com>
2025-02-22 14:44:53 +0530

    Made some changes to check

---
+++ src/main/java/com/HealQueue/CLINIC/Controller/ClinicController.java
@@ -0,0 +1,10 @@
+package com.HealQueue.CLINIC.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/clinic")
+public class ClinicController {
+
+}
