a95dcb50758cbae22316ddfc04bd797004bf7559
Abhinav Dwivedi <photon278@gmail.com>
2025-02-22 15:03:46 +0530

    Added

--- src/main/java/com/HealQueue/CLINIC/Controller/ClinicController.java
+++ src/main/java/com/HealQueue/CLINIC/Controller/ClinicController.java
@@ -1,10 +1,16 @@
 package com.HealQueue.CLINIC.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/clinic")
 public class ClinicController {
+
+    @GetMapping("/clinicData")
+    public String getDataById(){
+        return "";
+    }
 
 }
