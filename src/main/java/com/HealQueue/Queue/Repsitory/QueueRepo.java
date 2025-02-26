75b133ee3e1debe0f9b4cf0311101d593826dcee
Abhinav Dwivedi <photon278@gmail.com>
2025-02-26 21:06:27 +0530

    Nothing is changed

--- src/main/java/com/HealQueue/Queue/Repsitory/QueueRepo.java
+++ src/main/java/com/HealQueue/Queue/Repsitory/QueueRepo.java
@@ -2,6 +2,7 @@ package com.HealQueue.Queue.Repsitory;
 
 import com.HealQueue.Queue.Model.AppointmentBooking;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
 
 @Repository
