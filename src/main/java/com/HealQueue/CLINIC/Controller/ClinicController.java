3c18d80e1ce84d95e7c256afdd7d1548a6def846
Abhinav Dwivedi <photon278@gmail.com>
2025-02-26 21:29:42 +0530

    Added the queue showing method

--- src/main/java/com/HealQueue/CLINIC/Controller/ClinicController.java
+++ src/main/java/com/HealQueue/CLINIC/Controller/ClinicController.java
@@ -3,6 +3,8 @@ package com.HealQueue.CLINIC.Controller;
 import com.HealQueue.Auth.Entity.ClinicInfo;
 import com.HealQueue.Auth.Service.ClinicService;
 import com.HealQueue.CLINIC.DTO.ClinicResponse;
+import com.HealQueue.Queue.Model.AppointmentBooking;
+import com.HealQueue.Queue.Service.QueueService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,6 +22,9 @@ public class ClinicController {
     @Autowired
     private ClinicService clinicService;
 
+    @Autowired
+    private QueueService queueService;
+
     @GetMapping("/clinicData/{id}")
     public ResponseEntity<ClinicResponse> getDataById(@PathVariable long id){
         ClinicInfo clinicInfo = clinicService.findByData(id);
@@ -31,6 +36,12 @@ public class ClinicController {
         } catch (RuntimeException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    @GetMapping("/queue/get")
+    public ResponseEntity<List<AppointmentBooking>> getQueue(){
+        List<AppointmentBooking> apbook = queueService.getAllQueue();
+        return new ResponseEntity<>(apbook,HttpStatus.OK);
     }
 
 }
