19506fbb00d5979d689fdfa68e7b280832220897
Abhinav Dwivedi <photon278@gmail.com>
2025-05-02 00:34:19 +0530

    m

--- src/main/java/com/HealQueue/USER/Controller/UserDetailController.java
+++ src/main/java/com/HealQueue/USER/Controller/UserDetailController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/user")
 public class UserDetailController {
-
     @Autowired
     private UserService userService;
 
