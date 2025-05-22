d040d69bdbb066e2a867a0657f583a734b2c951d
Abhinav Dwivedi <photon278@gmail.com>
2025-05-23 01:39:39 +0530

    Added the api to send data to the backend

--- lib/API/UserAPI.dart
+++ lib/API/UserAPI.dart
@@ -1,20 +1,25 @@
-
 import 'dart:convert';
-
 import 'package:healfast01/Models/userModel.dart';
 import 'package:http/http.dart' as http;
 class UserCall{
   final String? baseUrl = 'http://192.168.0.105:8080/api';
 
   Future<void> addUser(UserModel user) async {
+    print(user.phoneNo);
+    print(user.firstName);
+    print(user.userName);
+
     final res = await http.post(
       Uri.parse("$baseUrl/register/user"),
-      headers: {'Content-type': 'application/json'},
+      headers: {'Content-Type': 'application/json'},
       body: jsonEncode(user.toJson()),
     );
-    if(res.statusCode == 200 || res.statusCode == 201){
-      print('EveryThing is fine');
-    }else{
+    print(res.body);
+    if (res.statusCode == 200 || res.statusCode == 201) {
+      print('Registration successful');
+    } else if (res.statusCode == 409) {
+      print('Username already exists');
+    } else{
       print('Status Code: ${res.statusCode}');
       print('Response Body: ${res.body}');
       throw Exception("Failed to register user");
