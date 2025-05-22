bd96468bfd55779c1f6fe2d9c2235b118b35debd
Abhinav Dwivedi <photon278@gmail.com>
2025-05-23 01:41:31 +0530

    Created a separate route files to manage routes efficiently

--- lib/Routes/Routes.dart
+++ lib/Routes/Routes.dart
@@ -1,9 +1,40 @@
-//
-// import 'package:flutter/material.dart';
-//
-// class Routes{
-//
-//   static Route<dynamic> generateRoutes(RouteSettings settings){
-//
-//   }
-// }
+
+import 'package:flutter/material.dart';
+import 'package:healfast01/BottomNav/BottomNavigation.dart';
+import 'package:healfast01/Routes/RoutesName.dart';
+import 'package:healfast01/ScreenPages/DoctorsRegisterationPage.dart';
+import 'package:healfast01/ScreenPages/HomePage.dart';
+import 'package:healfast01/ScreenPages/Login.dart';
+import 'package:healfast01/ScreenPages/Profile.dart';
+import 'package:healfast01/ScreenPages/Splash_Screen.dart';
+import 'package:healfast01/ScreenPages/UserRegisteration.dart';
+
+class Routes{
+
+  static Route<dynamic> generateRoute(RouteSettings settings){
+    switch(settings.name){
+      case RoutesName.userRegister:
+        return MaterialPageRoute(builder: (context)=>UserRegisteration());
+      case RoutesName.clinicRegister:
+        return MaterialPageRoute(builder: (context)=>DoctorsRegisterationPage());
+      case RoutesName.clinicName:
+        return MaterialPageRoute(builder: (context)=>HomePage());
+      case RoutesName.homePage:
+        return MaterialPageRoute(builder: (context)=>HomePage());
+      case RoutesName.login:
+        return MaterialPageRoute(builder: (context)=>Login());
+      case RoutesName.profilePage:
+        return MaterialPageRoute(builder: (context)=>Profile());
+      case RoutesName.splashScreen:
+        return MaterialPageRoute(builder: (context)=>SplashScreen());
+      case RoutesName.bottomNavigation:
+        return MaterialPageRoute(builder: (context)=>BottomNavigation());
+      default:
+        return MaterialPageRoute(builder: (context){
+          return const Scaffold(
+            body: Center(child: Text("No page is present")),
+          );
+        });
+    }
+  }
+}
--- lib/Routes/RoutesName.dart
+++ lib/Routes/RoutesName.dart
@@ -1,4 +1,11 @@
 
 class RoutesName{
-
+  static const String userRegister = 'User_Register';
+  static const String clinicRegister = 'Clinic_Register';
+  static const String login = 'Login';
+  static const String homePage = 'Home_Page';
+  static const String profilePage = 'Profile_Page';
+  static const String clinicName = 'Clinic_Names';
+  static const String splashScreen = 'Slash_Screen';
+  static const String bottomNavigation = 'Bottom_Navigation';
 }
